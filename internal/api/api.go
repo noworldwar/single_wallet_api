d6ea13966a515d3f48c1690ac9dc6b261be680ba
Mike <Mike>
2021-06-29 14:09:16 +0800

    test

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,24 +2,25 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/noworldwar/single_wallet_api/internal/model"
 )
 
 func Validate(c *gin.Context) {
-	playerID := c.PostForm("playerID")
-	player_data, err := model.GetPlayer(playerID)
-	if err != nil {
-		c.JSON(400, gin.H{"message": "player not found"})
-	}
-	c.JSON(200, gin.H{"playerID": player_data.PlayerID, "nickname": player_data.Nickname, "currency": player_data.Currency, "test": player_data.Test, "time": player_data.Created})
+	c.JSON(200, gin.H{"playerID": "test1", "nickname": "test1", "currency": "RMB", "test": 0, "time": 1624944938})
+	// playerID := c.PostForm("playerID")
+	// player_data, err := model.GetPlayer(playerID)
+	// if err != nil {
+	// 	c.JSON(400, gin.H{"message": "player not found"})
+	// }
+	// c.JSON(200, gin.H{"playerID": player_data.PlayerID, "nickname": player_data.Nickname, "currency": player_data.Currency, "test": player_data.Test, "time": player_data.Created})
 }
 func GetBalance(c *gin.Context) {
-	playerID := c.PostForm("playerID")
-	player_data, err := model.GetPlayer(playerID)
-	if err != nil {
-		c.JSON(400, gin.H{"message": "player not found"})
-	}
-	c.JSON(200, gin.H{"balacne": player_data.Balance, "currency": player_data.Currency, "time": player_data.Created})
+	c.JSON(200, gin.H{"balacne": 3000000, "currency": "RMB", "time": 1624944938})
+	// playerID := c.PostForm("playerID")
+	// player_data, err := model.GetPlayer(playerID)
+	// if err != nil {
+	// 	c.JSON(400, gin.H{"message": "player not found"})
+	// }
+	// c.JSON(200, gin.H{"balacne": player_data.Balance, "currency": player_data.Currency, "time": player_data.Created})
 }
 
 func Debit(c *gin.Context) {
