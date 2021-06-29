059342cbde7be3fcef839a57a0cc80d1fac1302e
Mike <Mike>
2021-06-29 14:36:02 +0800

    test

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,40 +1,33 @@
 package api
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
-	"github.com/noworldwar/single_wallet_api/internal/model"
 )
 
 func Validate(c *gin.Context) {
-	playerID := c.PostForm("playerID")
-	fmt.Println(playerID)
-	player_data, _ := model.GetPlayer(playerID)
-	if player_data.PlayerID == "" {
-		c.JSON(400, gin.H{"message": "player not found"})
-	}
-	var test_bool bool
-	if player_data.Test == 0 {
-		test_bool = false
-	} else {
-		test_bool = true
-	}
-	rsp := gin.H{"playerID": player_data.PlayerID, "nickname": player_data.Nickname, "currency": player_data.Currency, "test": test_bool, "time": player_data.Created}
+	c.JSON(200, gin.H{"playerID": "mike", "nickname": "Mike", "currency": "RMB", "test": false, "time": 1624944938})
+	// playerID := c.PostForm("playerID")
+	// player_data, err := model.GetPlayer(playerID)
+	// if err != nil {
+	// 	c.JSON(400, gin.H{"message": "player not found"})
+	// }
+	// var test_bool bool
+	// if player_data.Test == 0 {
+	// 	test_bool = false
+	// } else {
+	// 	test_bool = true
+	// }
 
-	fmt.Println(rsp)
-	c.JSON(200, rsp)
+	// c.JSON(200, gin.H{"playerID": player_data.PlayerID, "nickname": player_data.Nickname, "currency": player_data.Currency, "test": test_bool, "time": player_data.Created})
 }
 func GetBalance(c *gin.Context) {
-	playerID := c.PostForm("playerID")
-	fmt.Println(playerID)
-	player_data, _ := model.GetPlayer(playerID)
-	if player_data.PlayerID == "" {
-		c.JSON(400, gin.H{"message": "player not found"})
-	}
-	rsp := gin.H{"balacne": player_data.Balance, "currency": player_data.Currency, "time": player_data.Created}
-	fmt.Println(rsp)
-	c.JSON(200, rsp)
+	c.JSON(200, gin.H{"balacne": 30000000, "currency": "RMB", "time": 1624944938})
+	// playerID := c.PostForm("playerID")
+	// player_data, err := model.GetPlayer(playerID)
+	// if err != nil {
+	// 	c.JSON(400, gin.H{"message": "player not found"})
+	// }
+	// c.JSON(200, gin.H{"balacne": player_data.Balance, "currency": player_data.Currency, "time": player_data.Created})
 }
 
 func Debit(c *gin.Context) {
