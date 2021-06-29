4650da9c5b11745f4ff6d79d15c0007179b2addf
Mike <Mike>
2021-06-29 13:31:20 +0800

    Added some codes

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,12 +1,25 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/noworldwar/single_wallet_api/internal/model"
+)
 
 func Validate(c *gin.Context) {
-	c.JSON(200, gin.H{"playerID": "test1", "nickname": "test1", "currency": "RMB", "test": false, "time": 1574476825})
+	playerID := c.PostForm("playerID")
+	player_data, err := model.GetPlayer(playerID)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "player not found"})
+	}
+	c.JSON(200, gin.H{"playerID": player_data.PlayerID, "nickname": player_data.Nickname, "currency": player_data.Currency, "test": player_data.Test, "time": player_data.Created})
 }
 func GetBalance(c *gin.Context) {
-	c.JSON(200, gin.H{"balacne": 2000, "currency": "RMB", "time": 1574476825})
+	playerID := c.PostForm("playerID")
+	player_data, err := model.GetPlayer(playerID)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "player not found"})
+	}
+	c.JSON(200, gin.H{"balacne": player_data.Balance, "currency": player_data.Currency, "time": player_data.Created})
 }
 
 func Debit(c *gin.Context) {
