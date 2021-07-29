3145857fbf4bf80769734e5b8a3434820e85ba28
Mike <Mike>
2021-07-29 16:41:50 +0800

    changed betID

--- internal/model/transfer.go
+++ internal/model/transfer.go
@@ -4,7 +4,7 @@ type Transfer struct {
 	TransferID string  `json:"transferID" xorm:"varchar(255) pk"`
 	PlayerID   string  `json:"playerID"   xorm:"varchar(30) notnull"`
 	Type       string  `json:"type"   xorm:"varchar(20) notnull"`
-	BetID      string  `json:"betID"   xorm:"varchar(64) notnull unique"`
+	BetID      string  `json:"betID"   xorm:"varchar(64) notnull"`
 	GameID     string  `json:"gameID"   xorm:"varchar(64) notnull"`
 	Amount     float64 `json:"amount" xorm:"float notnull" `
 	Success    bool    `json:"success" xorm:"tinyint(1) notnull"`
